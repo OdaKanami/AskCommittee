61c23ad691ff8a5bccf195330f1ffcdc92dc28b6
kanami oda <k-oda@asking.co.jp>
2019-12-20 16:03:59 +0900

    pom.xml変更

---
+++ AskCommittee/src/main/java/com/example/demo/LoginController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class LoginController {
+
+@RequestMapping(value="/login")
+private String hello(){
+return "/login.html";
+}
+}
