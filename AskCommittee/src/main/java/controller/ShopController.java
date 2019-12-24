20165f783b16bf02c8339ca7acc814008b6a5886
kanami oda <k-oda@asking.co.jp>
2019-12-24 14:04:24 +0900

    form/ShopForm.java 作成

--- AskCommittee/src/main/java/controller/ShopController.java
+++ AskCommittee/src/main/java/controller/ShopController.java
@@ -2,27 +2,37 @@ package controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import form.ShopForm;
 
 @Controller
 public class ShopController {
 
-@RequestMapping(value="/shop")
-private String shopview(){
-return "/shop.html";
-}
+	@RequestMapping(value = "/shop")
+	private String shopview() {
+		return "/shop.html";
+	}
 
-@RequestMapping(value="/shop/register")
-private String register(){
-return "/shopRegister.html";
-}
+	@RequestMapping(value = "/shop/register", method = { RequestMethod.POST, RequestMethod.GET})
+	private String register() {
+		return "/shopregister.html";
+	}
 
-@RequestMapping(value="/shop/confirm")
-private String confirm(){
-return "/shopConfirm.html";
-}
+	@RequestMapping(value = "/shop/confirm", method = { RequestMethod.POST, RequestMethod.GET})
+	public ModelAndView confirm(ModelAndView mav, ShopForm ShopForm) {
+		ShopForm.setName("name");
+		ShopForm.setUrl("url");
+		ShopForm.setPhoneNumber("phoneNumber");
+		ShopForm.setRemark("remark");
+		mav.addObject("ShopForm", ShopForm);
+		mav.setViewName("/shop/comple");
+		return mav;
+	}
 
-@RequestMapping(value="/shop/comple")
-private String comple(){
-return "/shopComple.html";
-}
+	@RequestMapping(value = "/shop/comple")
+	private String comple() {
+		return "/shopcomple.html";
+	}
 }
---
+++ AskCommittee/src/main/java/form/ShopForm.java
@@ -0,0 +1,63 @@
+package form;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+public class ShopForm implements Serializable{
+
+	@NotEmpty(message = "店名が未入力です")
+	private String name;
+	
+	private String url;
+	
+	@Pattern(regexp = "0[0-9] {9,10}", message = "電話番号を正しく入力してください")
+	private String phoneNumber;
+	
+	private String remark;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+	
+	ShopForm(){
+		
+	}
+	
+	ShopForm(String name, String url, String phoneNumber, String remark){
+		this.name = name;
+		this.url = url;
+		this.phoneNumber = phoneNumber;
+		this.remark = remark;
+	}
+	
+}
