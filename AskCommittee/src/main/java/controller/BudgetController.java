9b175fcd7a41f1272a1e2ccbcce3410073f72c16
kanami oda <k-oda@asking.co.jp>
2020-01-21 17:20:46 +0900

    ドキドキpush

--- AskCommittee/src/main/java/controller/BudgetController.java
+++ AskCommittee/src/main/java/controller/BudgetController.java
@@ -1,23 +1,42 @@
 package controller;
 
+import java.util.List;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import form.BudgetForm;
-import form.ShopForm;
 
 @EnableAutoConfiguration
 @Controller
 public class BudgetController {
+	
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 
 	@RequestMapping(value = "/budget", method = RequestMethod.GET)
 	private String budgetview(Model model) {
+		List<Map<String,Object>> contentList;
+		contentList = jdbcTemplate.queryForList("select * from ask_budget_content");
+		List<Map<String,Object>> detailList;
+		detailList = jdbcTemplate.queryForList("select * from ask_budget_detail");
+		int total = jdbcTemplate.queryForObject("select sum(amount) from ask_budget_detail", Integer.class);
+		int balance = jdbcTemplate.queryForObject("SELECT bc.budget - SUM(bd.amount) AS calk\r\n" + 
+				"FROM askdb.ask_budget_content bc ,askdb.ask_budget_detail bd\r\n" + 
+				"WHERE bc.id = bd.detail_id", Integer.class);
+		model.addAttribute("total", total);
+		model.addAttribute("balance", balance);
+		model.addAttribute("ask_budget_content", contentList);
+		model.addAttribute("ask_budget_detail", detailList);
 		return "budget";
 	}
 
@@ -33,7 +52,7 @@ public class BudgetController {
 		if (result.hasErrors()) {
 			return "budgetchange";
 		}
-
+		
 		return "budgetconfirm";
 	}
 
