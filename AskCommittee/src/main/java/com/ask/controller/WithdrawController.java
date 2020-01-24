94dcc424f4f4a63b3183e0c6796465ba5883cefe
r-yamauchi <r-yamauchi@asking.co.jp>
2020-01-24 13:29:46 +0900

    withoutdraw

--- AskCommittee/src/main/java/com/ask/controller/WithdrawController.java
+++ AskCommittee/src/main/java/com/ask/controller/WithdrawController.java
@@ -1,15 +1,21 @@
 package com.ask.controller;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @EnableAutoConfiguration
 @Controller
 public class WithdrawController {
-	@RequestMapping(value="/withdraw", method = RequestMethod.GET)
-	private String withdrawview(){
+	@RequestMapping("/withdraw")
+	private String withdrawview(Model model){
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		String userName = auth.getName();
+		model.addAttribute("userName", userName);
 		return "withdraw";
 	}
 
