ec1cad6189bb6478bcf2ea96c97839aa029909c8
kanami oda <k-oda@asking.co.jp>
2020-01-24 17:52:17 +0900

    お店登録確認画面遷移　

--- AskCommittee/src/main/java/com/ask/controller/BudgetController.java
+++ AskCommittee/src/main/java/com/ask/controller/BudgetController.java
@@ -1,6 +1,7 @@
 package com.ask.controller;
 
 import java.awt.AWTException;
+
 import java.awt.Rectangle;
 import java.awt.Robot;
 import java.awt.image.BufferedImage;
@@ -25,7 +26,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.ask.form.BudgetForm;
+import com.ask.dto.BudgetRequest;
 
 @EnableAutoConfiguration
 @Controller
@@ -50,47 +51,6 @@ public class BudgetController {
 		model.addAttribute("ask_budget_detail", detailList);		
 		return "budget";
 	}
-	
-//	@RequestMapping(value = "/budget", method = {RequestMethod.POST, RequestMethod.GET})
-//	private String capture(Model model) {
-//		List<Map<String,Object>> contentList;
-//		contentList = jdbcTemplate.queryForList("select * from ask_budget_content");
-//		List<Map<String,Object>> detailList;
-//		detailList = jdbcTemplate.queryForList("select * from ask_budget_detail");
-//		int total = jdbcTemplate.queryForObject("select sum(amount) from ask_budget_detail", Integer.class);
-//		int balance = jdbcTemplate.queryForObject("SELECT bc.budget - SUM(bd.amount) AS calk\r\n" + 
-//				"FROM askdb.ask_budget_content bc ,askdb.ask_budget_detail bd\r\n" + 
-//				"WHERE bc.id = bd.detail_id", Integer.class);
-//		model.addAttribute("total", total);
-//		model.addAttribute("balance", balance);
-//		model.addAttribute("ask_budget_content", contentList);
-//		model.addAttribute("ask_budget_detail", detailList);
-//		
-//		System.setProperty("java.awt.headless", "false");
-//		
-//		SimpleDateFormat format = new SimpleDateFormat(
-//				"yyyyMMdd_HHmm");
-//		
-//		try {
-//			// キャプチャの範囲
-//			Rectangle bounds = new Rectangle(0, 0, 1280, 800);
-//
-//			// これで画面キャプチャ
-//			Robot robot = new Robot();
-//			BufferedImage image = robot.createScreenCapture(bounds);
-//
-//			// 以下、出力処理
-//			String dirName = "G:\\Project\\AskCommittee\\AskCommittee\\AskCommittee\\src\\main\\resources\\static\\img";
-//			String fileName = "test_" + format.format(new Date()) + ".jpg";
-//			ImageIO.write(image, "jpg", new File(dirName, fileName));
-//
-//		} catch (AWTException e) {
-//			e.printStackTrace();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//		return "budget";	
-//	}	 
 
 	@RequestMapping(value = "/budgetchange", method = RequestMethod.GET)
 	public String change(Model model) {
@@ -106,12 +66,12 @@ public class BudgetController {
 		model.addAttribute("balance", balance);
 		model.addAttribute("ask_budget_content", contentList);
 		model.addAttribute("ask_budget_detail", detailList);
-		model.addAttribute("budgetForm", new BudgetForm());
+		model.addAttribute("budgetRequest", new BudgetRequest());
 		return "budgetchange";		
 	}
 
 	@RequestMapping(value = "/budgetconfirm", method = RequestMethod.POST)
-	public String confirm(@ModelAttribute("budgetForm") BudgetForm budgetForm, BindingResult result, Model model) {
+	public String confirm(@ModelAttribute("budgetRequest") BudgetRequest budgetRequest, BindingResult result, Model model) {
 
 		if (result.hasErrors()) {
 			return "budgetchange";
@@ -122,7 +82,7 @@ public class BudgetController {
 	
 	@RequestMapping(value = "/budgetconfirm", method = RequestMethod.GET)
 	public String budgetconfirm(Model model) {
-		model.addAttribute("budgetForm", new BudgetForm());
+		model.addAttribute("budgetRequest", new BudgetRequest());
 		return "budgetconfirm";
 	}
 
