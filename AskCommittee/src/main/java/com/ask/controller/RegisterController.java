1fa41785e2d87cb6f0fcc7d453a0ec9c20625111
r-yamauchi <r-yamauchi@asking.co.jp>
2020-01-27 10:24:07 +0900

    新規会員登録

--- AskCommittee/src/main/java/com/ask/controller/RegisterController.java
+++ AskCommittee/src/main/java/com/ask/controller/RegisterController.java
@@ -1,22 +1,61 @@
 package com.ask.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.ask.entity.LoginUser;
 
 @EnableAutoConfiguration
 @Controller
 public class RegisterController {
+	
+	@Autowired
+    private JdbcUserDetailsManager userManager;
+	
+	/*
+	 * 会員情報入力画面
+	*/
 	@RequestMapping(value="/register", method = RequestMethod.GET)
 	private String registerview(){
 		return "register";
 	}
 	
+	@RequestMapping(value="/register", method = RequestMethod.POST)
+	public String register(@RequestParam("id") String id,
+			               @RequestParam("password") String name,
+			               @RequestParam("email") String email,
+						   @RequestParam("password") String password) {
+		
+//		UserBuilder users = User.withDefaultPasswordEncoder();
+//       userManager.createUser(users.username(id).password(password).email(email).roles("USER").build());
+		
+        return"";
+		
+	}	
+	
+	
+	
+	
+	
+	
+	//確認画面
 	@RequestMapping(value="/registerconfirm", method = RequestMethod.GET)
 	private String registerconfirmview(){
+		LoginUser entity = new LoginUser();
+		
+//		entity.setId(id);
+		
 		return "registerconfirm";
 	}
+	
+
 	
 	@RequestMapping(value="/registercomple", method = RequestMethod.GET)
 	private String registercompleview(){
--- AskCommittee/src/main/java/com/ask/controller/WithdrawController.java
+++ AskCommittee/src/main/java/com/ask/controller/WithdrawController.java
@@ -1,21 +1,27 @@
 package com.ask.controller;
 
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @EnableAutoConfiguration
 @Controller
 public class WithdrawController {
-	@RequestMapping("/withdraw")
-	private String withdrawview(Model model){
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		String userName = auth.getName();
-		model.addAttribute("userName", userName);
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	@RequestMapping(value="/withdraw", method = RequestMethod.GET)
+	private String withdrawview(){
+
 		return "withdraw";
 	}
 
