3b2171f64989e280d14209c476a507ec8b079d6e
r-yamauchi <r-yamauchi@asking.co.jp>
2020-01-31 17:49:20 +0900

    entity修正

--- AskCommittee/src/main/java/com/ask/entity/LoginUser.java
+++ AskCommittee/src/main/java/com/ask/entity/LoginUser.java
@@ -9,10 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
 
 @Entity
-@Data
 @Table(name = "Ask_member")
 public class LoginUser implements Serializable{
 	
@@ -29,5 +27,38 @@ public class LoginUser implements Serializable{
 	
 	@Column(name = "password")	
 	private String password;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	
 }
--- AskCommittee/src/main/java/com/ask/entity/Schedule.java
+++ AskCommittee/src/main/java/com/ask/entity/Schedule.java
@@ -7,10 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
 
 @Entity
-@Data
 @Table(name = "ask_schedule")
 public class Schedule {
 	
@@ -33,6 +31,55 @@ public class Schedule {
 	
 	@Column(name = "memo")
 	private String memo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+	
 	
 	
 }
