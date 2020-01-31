4a45d2359a208226e23f294a7e846f088a48a74c
kanami oda <k-oda@asking.co.jp>
2020-01-31 17:58:16 +0900

    リポジトリのエラー解決

--- AskCommittee/src/main/java/com/ask/entity/LoginUser.java
+++ AskCommittee/src/main/java/com/ask/entity/LoginUser.java
@@ -34,5 +34,34 @@ public class LoginUser implements Serializable{
 	public Integer getId() {
 		return id;
 	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	
 }
--- AskCommittee/src/main/java/com/ask/entity/Schedule.java
+++ AskCommittee/src/main/java/com/ask/entity/Schedule.java
@@ -32,6 +32,54 @@ public class Schedule {
 	private String tag;
 	
 	@Column(name = "memo")
-	private String memo;	
+	private String memo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getMemo() {
+		return memo;
+	}
+
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}	
 	
 }
